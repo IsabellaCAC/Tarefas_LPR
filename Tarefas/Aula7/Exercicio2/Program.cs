b1ce858cb32a05f75c5cf2c547b1ab1413edff3a
IsabellaCAC <>
2025-06-02 11:39:03 -0300

    exercicio2

---
+++ Tarefas/Aula7/Exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("-------- Bem vindo ao exercicio 2---------");
+Console.WriteLine("------------------------------------------");
+
+int[] numeros = new int[10];
+int buscado, ocorrencia;
+
+Console.WriteLine("Digite 10 numeros inteiros: ");
+
+for (int i = 0; i < 10; i++) {
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nDigite um numero para pesquisar no vetor: ");
+
+buscado = int.Parse(Console.ReadLine());
+
+ocorrencia = 0;
+
+Console.WriteLine("\n Procurando o numero: {buscado}");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] == buscado) {
+        Console.WriteLine("O numero buscado está na posição {i}.");
+        ocorrencia++;
+    }
+}
+if (ocorrencia > 0) {
+    Console.WriteLine("");
+    }
